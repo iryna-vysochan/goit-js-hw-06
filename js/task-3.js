22f0c55fd74099c3148f2337f9efc042c23ccfb7
Iryna Vysochan <ivysochan@gmail.com>
2025-09-14 11:48:46 +0100

    hm-06

--- js/task-3.js
+++ js/task-3.js
@@ -1,24 +1,37 @@
-const profile = {
-    username: "Jacob",
-  playTime: 300,
+class StringBuilder {
+  
+  #value;
 
-  changeUsername(newName){
-    this.username = newName;
-  },
-  updatePlayTime(hours){
-    this.playTime += hours;
-  },
-  getInfo(){
-    return `${this.username} has ${this.playTime} active hours!`;
+  constructor(initialValue) {
+    this.#value = initialValue;
   }
-};
+
+  getValue() {
+    return this.#value;
+  }
+
+  padEnd(str) {
+    this.#value += str;
+  }
+
+  padStart(str) {
+    this.#value = str + this.#value;
+  }
+
+  padBoth(str) {
+    this.#value = str + this.#value + str;
+  }
+}
 
 
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
 
-console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
 
-profile.changeUsername("Marco");
-console.log(profile.getInfo()); // "Marco has 300 active hours!"
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
 
-profile.updatePlayTime(20);
-console.log(profile.getInfo()); // "Marco has 320 active hours!"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
